2a86e4ec02bc1384bf6c818a065955041cd15af1
sasha <nasdvoya@proton.me>
2024-08-09 11:11:55 +0100

    Solution setup

---
+++ src/ExampleClassLib/Foundation.cs
@@ -0,0 +1,13 @@
+﻿namespace ExampleClassLib;
+
+public class Foundation
+{
+    public int FoundationSize { get; set; }
+    public string FoundationName { get; set; }
+
+    public Foundation(int foundationSize, string foundationName)
+    {
+        this.FoundationSize = foundationSize;
+        this.FoundationName = foundationName;
+    }
+}
---
+++ src/ExampleWorker/Worker.cs
@@ -0,0 +1,31 @@
+using ExampleClassLib;
+using Newtonsoft.Json;
+
+namespace ExampleWorker;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var foundation = new Foundation(10, "Oak");
+        var json = JsonConvert.SerializeObject(foundation);
+
+        _logger.LogInformation("Foundation: {Json}", json);
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
